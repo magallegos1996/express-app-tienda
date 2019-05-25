9d4b08edcfbc143b2c5938f4656e41949b59c74d
magallegos1996 <bwcekos@gmail.com>
2019-05-25 18:47:07 -0500

    Se configura el puerto de forma dinámica

--- src/app.js
+++ src/app.js
@@ -6,5 +6,6 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+app.set('port', process.env.PORT || 4000); //Se configura el puerto en el se va a iniciar el servidor de forma dinámica. Tomará el puerto del SO y si no hay usará el puerto por defecto, en este caso el 4000
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-const express = require('express');
 const app = require('./app');
 
 
 async function main() {
     //Inicial servidor Express
-    await app.listen(4000);
-    console.log('Server on port 4000: connected')
+    await app.listen(app.get('port'));
+    console.log('Server on port ' + app.get('port') + ': connected' );
 }
 
 //llamamos a la funcion
