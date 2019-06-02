53bea998a9719809a92511e5d63c4d18b70c7f84
Marcelo Gallegos <bwcekos@gmail.com>
2019-06-02 17:45:31 -0500

    Se implementan control de errores

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -3,37 +3,39 @@ const Categoria = require('../models/CategoriaProducto');
 const Producto = require('../models/Producto');
 
 categoriaController.getCategorias = async (req,res)=>{
-    const categorias =  await Categoria.find();
-    res.json(categorias);
+    try {
+        const categorias =  await Categoria.find();
+        res.json(categorias);
+    }catch (e) {res.json({status: 'Error'})}
 };
 categoriaController.getCategoriaByNombre = async (req, res)=>{
-    const categoria = await Categoria.findOne({nombre: req.params.nombre});
-    res.json(categoria);
+    try{
+        const categoria = await Categoria.findOne({nombre: req.params.nombre});
+        res.json(categoria);
+    } catch (e) {res.json({status: 'Error'})}
 };
 categoriaController.crearCategoria = async (req, res)=>{
     const nuevaCategoria = new Categoria(req.body);
-    await nuevaCategoria.save();
-    res.json(nuevaCategoria)
+    try {
+        await nuevaCategoria.save();
+        res.json(nuevaCategoria)
+    }catch (error) {res.json({status:'Error'})}
 };
-categoriaController.eliminarCategoria = (req, res)=>{
+categoriaController.eliminarCategoria = async (req, res)=>{
     Categoria.findById(req.params.id, (err, categoria)=>{
-        if(err) throw err;
         Producto.remove({
-            "_id":{
+            '_id': {
                 $in: categoria.productos
             }
         }, (err)=>{
-            if(err) throw err;
+            if(err) res.json({status: 'Error'});
             categoria.remove();
             res.json({status: 'Categoria Eliminada'});
+
         })
     });
-   // res.json({staus: 'Categoria Eliminada'});
+    // res.json({staus: 'Categoria Eliminada'});
 };
-
-
-
-
 
 module.exports = categoriaController;
 
